ecd6e21e5b53cc12f679b02623210434d5bba9ca
Adeoti suliat Adedamola <loshdami@gmail.com>
2025-07-15 09:36:23 +0100

    first commit

---
+++ recipes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField()
+    author = models.CharField()
+    content = models.TextField()
+    ingredient = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
---
+++ recipes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from . models import Recipe
+
+# Create your views here.
+def index(request):
+    recipes = Recipe.objects.all()
+    context = {'recipes': recipes}
+    template = 'recipes/index.html'
+    return render (request, template, context)
